6103e154158fa38c744d7617b747e77c3f288ec6
hupfei <HG_hupfei@163.com>
2018-12-29 14:05:09 +0800

    Initial Commit

---
+++ HGDownloadManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HGDownloadManager
+//
+//  Created by hupfei on 2018/12/29.
+//  Copyright © 2018 hupfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
